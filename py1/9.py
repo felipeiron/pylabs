21e1ee6046376a6ffd49425862c0ffa92470c9d5
felipeiron <gleydson.felipe610@gmail.com>
2021-09-08 17:00:09 -0300

    push resolved

--- py1/9.py
+++ py1/9.py
@@ -5,4 +5,4 @@ print('##### CONVERTE TEMPERATURA #####\n')
 f = float(input('Qual a temperatura em Fahrenheit? '))
 c = 5 * ((f - 32) / 9)
 
-print('\nA temperatura {:.2f}° equivale ao valor digitado em Fahrenheit,'.format(c))
+print('\nA temperatura {:.2f}°C equivale ao valor digitado em Fahrenheit,'.format(c))
