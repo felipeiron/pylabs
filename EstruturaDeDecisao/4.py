472ab51d5820eaf0fd201a9624594c7c16622ca7
Gleydson Felipe <gleydson.felipe610@gmail.com>
2021-09-27 21:48:47 -0300

    adjustment done

--- EstruturaDeDecisao/4.py
+++ EstruturaDeDecisao/4.py
@@ -4,9 +4,8 @@ os.system('clear')
 print('##### VOGAL/CONSOANTE #####\n')
 letra = input('Digite uma letra: ')
 
-if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-    print('\nÉ uma vogal!\n')
-elif letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+
+if letra.capitalize() == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
     print('\nÉ uma vogal!\n')
 else:
     print('\nÉ uma consoante!\n')
--- EstruturaDeDecisao/7.py
+++ EstruturaDeDecisao/7.py
@@ -1,4 +1,28 @@
 import os
-os.system('cls')
+os.system('clear')
 
 print('##### MAIOR E MENOR #####\n')
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+n3 = float(input('Terceiro número: '))
+
+if n1 > n2 and n1 > n3:
+    print('\nO maior número é {}'.format(n1))
+    if n2 < n3:
+        print('O menor número é {}\n'.format(n2))
+    else:
+        print('O menor número é {}\n'.format(n3))
+elif n2 > n1 and n2 > n3:
+    print('\nO maior número é {}'.format(n2))
+    if n1 < n3:
+        print('O menor número é {}\n'.format(n1))
+    else:
+        print('O menor número é {}\n'.format(n3))
+elif n3 > n1 and n3 > n2:
+    print('\nO maior número é {}'.format(n3))
+    if n1 < n2:
+        print('O menor número é {}\n'.format(n1))
+    else:
+        print('O menor número é {}\n'.format(n2))
+else:
+    print('\nTodos números são iguais!!!\n')
