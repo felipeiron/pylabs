0214823af5f8be33e5b312d823e06d9d5ecdb5e9
Gleydson Felipe <gleydson.felipe610@gmail.com>
2021-10-22 23:06:27 -0300

    Updating what the code show after calculate average

--- EstruturaDeDecisao/14.py
+++ EstruturaDeDecisao/14.py
@@ -2,19 +2,19 @@ import os
 os.system('clear')
 
 print('##### MÉDIA E CONCEITO #####\n')
-nota = float(input('Sua primeira nota: '))
-nota += float(input('Sua segunda nota: '))
-media = nota / 2
+nota1 = float(input('Sua primeira nota: '))
+nota2 = float(input('Sua segunda nota: '))
+media = (nota1 + nota2) / 2
 
 if media > 9.0 and media <= 10.0:
-    print('\nA\n')
+    print('\nSuas notas foram {} e {}, e a média é {} - Conceito A - Aprovado\n'.format(nota1, nota2, media))
 elif media > 7.5 and media <= 9.0:
-    print('\nB\n')
+    print('\nSuas notas foram {} e {}, e a média é {} - Conceito B - Aprovado\n'.format(nota1, nota2, media))
 elif media > 6.0 and media <= 7.5:
-    print('\nC\n')
+    print('\nSuas notas foram {} e {}, e a média é {} - Conceito C - Aprovado\n'.format(nota1, nota2, media))
 elif media > 4.0 and media <= 6.0:
-    print('\nD\n')
+    print('\nSuas notas foram {} e {}, e a média é {} - Conceito D - Reprovado\n'.format(nota1, nota2, media))
 elif media >= 0 and media <= 4.0:
-    print('\nE\n')
+    print('\nSuas notas foram {} e {}, e a média é {} - Conceito E - Reprovado\n'.format(nota1, nota2, media))
 else:
     print('\nValores muito altos!!!\n')
