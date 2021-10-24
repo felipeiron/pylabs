67acd284c597635199d4a479eb060cd594ab5029
Gleydson Felipe <gleydson.felipe610@gmail.com>
2021-10-24 15:01:29 -0300

    Add comments about first if

--- EstruturaDeDecisao/17.py
+++ EstruturaDeDecisao/17.py
@@ -4,6 +4,8 @@ os.system('clear')
 print('##### ANO BISSEXTO #####\n')
 ano = int(input('Informe o ano: '))
 
+#A primeira parte do if testa anos que sejam uniformemente divisiveis por 100 seguinto a regra do calendário gregoriano.
+#Caso esse teste seja negativo ele testa o elif, e caso este também seja negativo vai pro else.
 if ano % 100 == 0:
     if ano % 400 == 0:
         if ano % 4 == 0:
