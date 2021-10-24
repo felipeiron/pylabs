3803ec86ab4463a56163e6a1a2d9e9def60f2046
Gleydson Felipe <gleydson.felipe610@gmail.com>
2021-10-24 19:30:26 -0300

    uploading exercise 23

---
+++ EstruturaDeDecisao/23.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+os.system('clear')
+
+print('##### INTEIRO ou DECIMAL #####\n')
+num = float(input('Digite um número: '))
+
+if num == round(num):
+    print('\nO número {} é inteiro.\n'.format(num))
+else:
+    print('\nO número {} é decimal.\n'.format(num))
+
