cc1708f2aa53914f405f54ae27a45d66ba3eb719
gleydson <gleydson.felipe610@gmail.com>
2022-11-13 21:03:50 -0300

    I did a update

--- EstruturaDeRepetição/2.py
+++ EstruturaDeRepetição/2.py
@@ -1,14 +1,14 @@
-import os
-
-os.system('cls')
-
-print('##### USER/PASSWORD #####\n')
+import os, time
 
 while True:
+    os.system('clear')
+    print('##### USER/PASSWORD #####\n')
     user = input('What is your username: ')
     passw = input('What is your password: ')
     if passw.upper() == user.upper():
         print('\nYour password cannot be the same as the username. ')
         print('Please, repeat the process!\n')
+        time.sleep(3)
     else:
+        print('\nProcess finished!\n')
         break
