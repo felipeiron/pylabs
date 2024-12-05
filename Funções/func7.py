3aaca40c554df8a48a1db0b2e57fcdd9be37f28e
Gleydson França <gleydson.felipe610@gmail.com>
2024-12-05 20:27:06 -0300

    Exercicio finalizado

--- Funções/func7.py
+++ Funções/func7.py
@@ -18,10 +18,16 @@ def resultado(parcela, valor_atraso):
 #Entrada e saída de dados
 
 prestacoes = 100.0
+total = 0
+contador = 0
 while prestacoes > 0:
     os.system('clear')
-    valor_prestacao = float(input('Digite: '))
+    valor_prestacao = float(input('Digite o valor da parcela: '))
     dias_atraso = int(input('Quantos dias em atraso: '))
     prestacoes = valor_prestacao
+    total += valor_prestacao
+    contador += 1
     resultado(valor_prestacao, dias_atraso)
     time.sleep(5)
+else:
+    print(f'\nPrestações pagas: {contador}\nTotal pago: {total}')
